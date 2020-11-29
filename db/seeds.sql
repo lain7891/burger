f2086077b02f0b9024ea873c1f147296bc11f5e7
Estefania <nialupo87@gmail.com>
2020-11-29 18:36:20 -0500

    added the insert queries to the seed file

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Original Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Blue Shroom Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Holy Guacamole Burger', true);
+
+SELECT * FROM burgers;
