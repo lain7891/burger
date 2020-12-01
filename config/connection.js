60b5bda606f5a84946cb26efc9f240401fdab1a3
Estefania <nialupo87@gmail.com>
2020-12-01 15:00:42 -0500

    working on the orm's

--- config/connection.js
+++ config/connection.js
@@ -16,13 +16,13 @@ var connection;
 
   connection.connect();
   
-//   connection.connect(function(err) {
-//     if (err) {
-//       console.error("error connecting: " + err.stack);
-//       return;
-//     }
-//     console.log("connected as id " + connection.threadId);
-//   });
+  connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
   
   // Export connection for our ORM to use.
   module.exports = connection;
