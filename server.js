da60d9f556bdd933c5f047fe20cfaad0dbcd4567
Estefania <nialupo87@gmail.com>
2020-12-01 15:04:28 -0500

    set server.js file with setting the handlebars, importing routes, starting server

--- server.js
+++ server.js
@@ -1,2 +1,26 @@
 
 const exphbs = require("express-handlebars");
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/catsController.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
