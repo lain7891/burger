64aef2a9e18c3412cd4cbd03777a94fbb7740721
Estefania <nialupo87@gmail.com>
2020-12-03 23:25:31 -0500

    update image on the index.handlebars

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,23 +1,46 @@
 $(function() {
-    $(".create-form").on("submit", function(event) {
-        event.preventDefault();
-    //   var id = $(this).data("id");
-    //   var newSleep = $(this).data("newsleep");
+    $(".change-devour").on("click", function(event) {
+      var id = $(this).data("id");
+      var newBurger = $(this).data("newburger");
   
-      var newDevour= {
-        devour: newDevour
+      var newDevourState = {
+        devoured: newDevoured,
       };
   
       // Send the PUT request.
       $.ajax("/api/burgers/" + id, {
         type: "PUT",
-        data: newDevour
+        data: newDevourState,
       }).then(
         function() {
-          console.log("changed burger to", newDevour);
+          console.log("changed devour to", newDevour);
           // Reload the page to get the updated list
           location.reload();
         }
       );
     });
-});
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#ca").val().trim(),
+        devoured: $("[name=devoured]:checked").val().trim(),
+      };
+    
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger,
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
